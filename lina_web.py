b0310377e160bf23976efbc4e8e6d7e13ed6ac12
germalem-eng <germalem@gmail.com>
2026-03-28 14:10:45 -0500

    Update lina_web.py

--- lina_web.py
+++ lina_web.py
@@ -90,31 +90,27 @@ col_izq, col_der = st.columns([1.2, 2.3])
 with col_izq:
     st.markdown(f'<div style="display:flex; justify-content:center; align-items:center; height:100%;"><img src="data:image/jpeg;base64,{logo_robot_b64}" class="logo-redondo-final"></div>', unsafe_allow_html=True)
 
-with col_der:
-    st.markdown(f"""
-    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center; margin-top: -30px;">
+st.markdown("""
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center; margin-top: -30px; width: 100%;">
+        <h1 class="neon-imponente" style="font-size: 110px; margin: 0; padding: 0; line-height: 0.85;">L.I.N.A.</h1>
         
-        <h1 class="neon-imponente" style="font-size: 115px; margin: 0; padding: 0; line-height: 0.85;">L.I.N.A.</h1>
-        
-        <div style="display: flex; flex-direction: column; align-items: center; gap: 4px;">
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 5px; width: 100%;">
             <div class="resaltado-blanco">
-                <span style="color: #008fb3; font-size: 28px; font-weight: bold;">Laboratorio de Inteligencia</span>
+                <span style="color: #008fb3; font-size: 26px; font-weight: bold;">Laboratorio de Inteligencia</span>
             </div>
             <div class="resaltado-blanco">
-                <span style="color: #008fb3; font-size: 28px; font-weight: bold;">y Nuevos Algoritmos</span>
+                <span style="color: #008fb3; font-size: 26px; font-weight: bold;">y Nuevos Algoritmos</span>
             </div>
             
-            <div style="margin-top: 8px;" class="resaltado-blanco">
+            <div class="resaltado-blanco" style="margin-top: 10px;">
                 <span style="color: #444; font-size: 18px; font-weight: bold;">Soluciones Tecnológicas M Y M</span>
             </div>
             <div class="resaltado-blanco">
                 <span style="color: #444; font-size: 16px; font-weight: bold;">Desde 2007</span>
             </div>
         </div>
-        
     </div>
-    """, unsafe_allow_html=True)
-    
+    """, unsafe_allow_html=True)    
 # --- 7. LÓGICA DE SECCIONES (ACTUALIZADO) ---
 
 if st.session_state.seccion == "COTIZADOR":
